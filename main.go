76b1fe81e55a50ca08a8bb73501dce58458955db
jonathan wilson <jonathan@revcontent.com>
2025-06-25 12:19:57 -0600

    mem profile activated

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"math/rand/v2"
 	"os"
+	"runtime"
 	"runtime/pprof"
 	"strconv"
 	"sync"
@@ -95,6 +96,19 @@ func main() {
 		log.Fatal("could not start CPU profile: ", err)
 	}
 	defer pprof.StopCPUProfile()
+
+	mf, e := os.Create("mem.prof")
+	if e != nil {
+		log.Fatal("could not create memory profile: ", err)
+	}
+	defer mf.Close() // error handling omitted for example
+	runtime.GC()     // get up-to-date statistics
+	// Lookup("allocs") creates a profile similar to go test -memprofile.
+	// Alternatively, use Lookup("heap") for a profile
+	// that has inuse_space as the default index.
+	if err := pprof.Lookup("allocs").WriteTo(mf, 0); err != nil {
+		log.Fatal("could not write memory profile: ", err)
+	}
 
 	// Maximum number of entries in the sturdyc.
 	capacity := 300000
